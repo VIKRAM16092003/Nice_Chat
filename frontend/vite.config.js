f383a5c3bbdade3bfd0ee298aa9d0fdf1a329634
VIKRAM1692003 <vikram1692003@gmail.com>
2025-03-17 19:33:37 +0530

    update 7

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),], build: {
-    chunkSizeWarningLimit: 1000, // Increases the limit to 1000kB
-
-}  });
+  plugins: [react(),tailwindcss(),],
+});
