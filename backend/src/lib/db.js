7bbcd9e078af05c9511eb5fc194cffa13bbe5525
VIKRAM1692003 <vikram1692003@gmail.com>
2025-03-17 19:19:25 +0530

    update 6

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB= async () => {
     try {
-        const conn=await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            maxPoolSize: 10, // Limit MongoDB connections
-          });
+        const conn=await mongoose.connect(process.env.MONGODB_URI);
         console.log("MONGODB connect "+conn.connection.host);
         
     } catch (error) {
